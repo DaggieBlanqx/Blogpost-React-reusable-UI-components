418cd581d2ca96e0cdfcbb2ad894d97b7b5a6c7e
Daggie Blanqx (Douglas Mwangi) <daggieblanqx@gmail.com>
2023-05-22 19:04:30 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,110 @@
 // ./index.jsx
-import React, {useState} from "react";
+import { useState } from "react";
 import FormInput from "./components/FormInput.jsx";
 import CustomSelect from "./components/CustomSelect.jsx";
-import ToggleSwitch from "./components/ToggleSwitch.jsx";
 import Button from "./components/Button.jsx";
-import Modal from "./components/Modal.jsx";
 import FormWrapper from "./components/FormWrapper.jsx";
 import SearchBar from "./components/SearchBar.jsx";
-import Dialog from "./components/Dialog.jsx";
+import Modal from "./components/Modal.jsx";
+import ToggleSwitch from './components/ToggleSwitch.jsx';
 
 function App() {
-    const [formTwoSearch, modify_formTwoSearch] = useState("");
-    const [modalStatus, modify_modalStatus] = useState(false);
-    const [isModalShowing, setIsModalShowing] = useState(false);
+  const [formTwoSearch, modify_formTwoSearch] = useState("");
+  const [isModalShowing, setIsModalShowing] = useState(false);
 
-    const data = [
-        {
-            id: "1",
-            name: "One",
-        },
-        {
-            id: "2",
-            name: "Two",
-        },
-        {
-            id: "3",
-            name: "Three",
-        },
-        {
-            id: "4",
-            name: "Four",
-        },
-    ];
+  const data = [
+    {
+      id: "1",
+      name: "One",
+    },
+    {
+      id: "2",
+      name: "Two",
+    },
+    {
+      id: "3",
+      name: "Three",
+    },
+    {
+      id: "4",
+      name: "Four",
+    },
+  ];
 
-    function handleChange(event) {
-        console.log(event.target.value);
-    }
+  function handleChange(event) {
+    console.log(event.target.value);
+  }
 
-    function onToggleChange(event) {
-        console.log(event.target.checked);
-    }
+  function onToggleChange(status) {
+    console.log({ toggle: status })
+  }
 
-    const formOneSubmitter = ({event}) => {
-        console.log("Form One can be submitted");
-        event.preventDefault();
-    };
+  const formOneSubmitter = ({ event }) => {
+    console.log("Form One can be submitted");
+    event.preventDefault();
+  };
 
-    const formTwoSubmitter = ({event}) => {
-        console.log("Form Two can be submitted");
-        event.preventDefault();
-    };
+  const formTwoSubmitter = ({ event }) => {
+    console.log("Form Two can be submitted");
+    event.preventDefault();
+  };
 
-    return (
-        <>
-            <FormWrapper onSubmit={formOneSubmitter} title="Form One">
-                <ToggleSwitch
-                    id="id"
-                    defaultChecked={false}
-                    disabled={false}
-                    Text={["Yes", "No"]}
-                    onToggleChange={onToggleChange}
-                />
-                <CustomSelect
-                    data={data}
-                    title="Select number of bedrooms"
-                    onSelectChange={(event) => console.log(`Got ${event.target.value}`)}
-                />
-                <br />
-                <FormInput type={"email"} onChange={handleChange} />
-                &nbsp;
-                <Button type="submit" variant="primary" size={"lg"}>
-                    Submit data
-                </Button>
-            </FormWrapper>
+  return (
+    <>
+      <FormWrapper onSubmit={formOneSubmitter} title="Form One">
+        
+        <CustomSelect
+          data={data}
+          title="Select number of bedrooms"
+          onSelectChange={(event) => console.log(`Got ${event.target.value}`)}
+        />
 
-            <FormWrapper onSubmit={formTwoSubmitter} title="Form Two">
-                <SearchBar
-                    placeHolder="Find a house near me"
-                    onSearch={(searchTerm) => modify_formTwoSearch(searchTerm)}
-                />
-                &nbsp;
-                <Button type="reset" variant="warning" size={"sm"}>
-                    Clear Form
-                </Button>
-                {formTwoSearch && (
-                    <p>
-                        Searching for "<b>{formTwoSearch}</b>"
-                    </p>
-                )}
-            </FormWrapper>
+        <FormInput title="Email address" type={"email"} onChange={handleChange} />
+        
+        <FormInput title="Enter password" type={"password"} onChange={handleChange} />
+       
+        
+        <ToggleSwitch disabled={false} defaultChecked={true} onToggleChange={onToggleChange} OnTitle="Agree" OffTitle="Disagree" />
 
-            <>
-                <Button onClick={() => setIsModalShowing(true)}>View modal</Button>
-                <Dialog isShowing={isModalShowing} onClose={() => setIsModalShowing(false)}>
-                    <h3>About this app</h3>
-                    <p>
-                        Here's how to use functional components & hooks (e.g useState, useRef etc)
-                        to create reusable components.
-                    </p>
-                    <Button>
-                        <a href="https://blog.logrocket.com/author/daggieblanqx/">
-                            See more articles
-                        </a>
-                    </Button>
-                    &nbsp;
-                    <Button onClick={() => setIsShowing(false)}>Close Modal</Button>
-                </Dialog>
-            </>
-        </>
-    );
+        
+        <Button type="submit" variant="primary" size={"lg"}> Submit data </Button>
+      </FormWrapper>
+
+      <FormWrapper onSubmit={formTwoSubmitter} title="Form Two">
+        <SearchBar
+          placeHolder="Find a house near me"
+          onSearch={(searchTerm) => modify_formTwoSearch(searchTerm)}
+        />
+        &nbsp;
+        <Button type="reset" variant="warning" size={"sm"}>
+          Clear Form
+        </Button>
+        
+        {formTwoSearch && (
+          <p>
+            Searching for "<b>{formTwoSearch}</b>"
+          </p>
+        )}
+      </FormWrapper>
+
+      <>
+        <Button onClick={() => setIsModalShowing(true)}>View modal</Button>
+        <Modal isShowing={isModalShowing} onClose={() => setIsModalShowing(false)}>
+          <h3>About this app</h3>
+          <p>
+            Here's how to use functional components & hooks (e.g useState, useRef etc)
+            to create reusable components.
+          </p>
+          <Button>
+            <a href="https://blog.logrocket.com/author/daggieblanqx/">
+              See more articles
+            </a>
+          </Button>
+          &nbsp;
+          <Button onClick={() => setIsModalShowing(false)}>Close Modal</Button>
+        </Modal>
+      </>
+    </>
+  );
 }
 export default App;
